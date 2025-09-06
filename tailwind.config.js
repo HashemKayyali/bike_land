8f848c69ebbfa5d8d9b1a510b6dc0637951e5755
HashemKayyali <hashemkayyali99@gmail.com>
2025-09-06 13:12:46 +0300

    update site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: ["./index.html", "./**/*.{html,js}", "!./node_modules/**"],
-theme: { extend: {} },
-plugins: []
+  content: [
+    "./index.html",
+    "./**/*.{html,js}",
+    "./node_modules/preline/dist/*.js", // مهم
+  ],
+  theme: { extend: {} },
+  plugins: [], // احذف require('preline/plugin')
 }
